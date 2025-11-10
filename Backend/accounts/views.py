ebd2e460f122a5e3a22559eade5f3d3512184e36
KIWOLY <ikiwoly@gmail.com>
2025-11-10 14:37:14 +0300

    complete function of login and logout implimented by DRF and JWT

--- Backend/accounts/views.py
+++ Backend/accounts/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response 
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken
 
-# Create your views here.
+
+# This view gives the user an access token + refresh token when they log in.
+class LoginView(TokenObtainPairView):
+    permission_classes = [AllowAny] 
+    
+
+
+# This view invalidates (blacklists) the refresh token when the user logs out.
+class LogoutView(APIView):
+    def post(self, request):
+        try:
+            refresh_token = request.data["refresh"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()  
+            return Response({"message": "Successfully logged out"}, status=status.HTTP_205_RESET_CONTENT)
+        except Exception:
+            return Response({"error": "Invalid or expired token"}, status=status.HTTP_400_BAD_REQUEST)
