8718eb25304318a99659e5f4856315f2c95845e4
mwaitukasteven <mwaitukasteven@gmail.com>
2026-01-20 23:35:46 +0300

    change system email

--- Backend/announcements/email_service.py
+++ Backend/announcements/email_service.py
@@ -6,28 +6,21 @@ User = get_user_model()
 def send_announcement_email(announcement):
     users = User.objects.filter(is_active=True)
 
-    recipient_list = [
-        user.email for user in users
-        if user.email
-    ]
+    for user in users:
+        if not user.email:
+            continue
 
-    if not recipient_list:
-        return
 
-    subject = f"📢 TLMS new Announcement: {announcement.title}"
-
-    content = f"""
-A new announcement has been posted by the system administrator.
-
-Title: {announcement.title}
-
-Please log in to the system for more details.
+    subject = f"📢TLMS, Hello {user.first_name}, New Announcement: {announcement.title}"
+    for user in users:
+        content = f"""
+Please login now for more details.
 """
 
-    send_mail(
-        subject,
-        content,
-        None,
-        recipient_list,
-        fail_silently=False
-    )
+        send_mail(
+            subject,
+            content,
+            None,
+            [user.email],
+            fail_silently=False
+        )
