429f579b1f17525ea690687800d82d37c6983a7d
KIWOLY <ikiwoly@gmail.com>
2026-02-23 20:06:01 +0300

    cahnge allowed host

--- Backend/team_system/settings.py
+++ Backend/team_system/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = os.getenv("DEBUG")
 
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1','	130.61.230.130', 'backend']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1','130.61.230.130', 'backend']
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
